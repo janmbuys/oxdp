41ba253302c154fbf4a8d464bf87312cfaafc502
Jan Buys <jan.buys@cs.ox.ac.uk>
2014-08-21 11:30:34 +0100

    Touched files

---
+++ src/hpyplm/hpyplm_conll.cc
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <cstdlib>
+#include <chrono>
+
+#include "hpyplm.h"
+#include "corpus/corpus.h"
+#include "pyp/m.h"
+#include "pyp/random.h"
+#include "pyp/crp.h"
+#include "pyp/tied_parameter_resampler.h"
+
+using namespace oxlm;
+
+typedef std::vector<WordIndex> WxList;
+typedef std::vector<Words> WordsList;
+
+int main(int argc, char** argv) {
+  const unsigned kOrder = 4;
+  MT19937 eng;
+
+  if (argc != 2) {
+      std::cerr << argv[0] << " <nsamples>\n";
+    return 1;
+  }
+
+  int num_samples = atoi(argv[1]);
+
+  Dict dict("<s>", "</s>");
+  const WordId kSOS = dict.convert("<s>", true);
+  const WordId kEOS = dict.convert("</s>", true);
+
+  /*Training */
+  std::string train_file = "english-wsj/english_wsj_train.conll";
+
+  std::vector<Words> corpus_sents;
+  std::vector<Words> corpus_tags;
+  std::vector<WxList> corpus_deps;
+  
+  std::cerr << "Reading training corpus...\n";
+  dict.readFromConllFile(train_file, &corpus_sents, &corpus_tags, &corpus_deps, false);
+  std::cerr << "Corpus size: " << corpus_sents.size() << " sentences\t (" << dict.size() << " word types, " << dict.tag_size() << " tags)\n";
+
+  //define pyp model
+  PYPLM<kOrder> lm(dict.size() + 1, 1, 1, 1, 1);
+ 
+  std::cerr << "\nStarting training\n";
+  auto tr_start = std::chrono::steady_clock::now();
+                  
+  std::vector<WordId> ctx(kOrder - 1, kSOS);
+  for (int sample = 0; sample < num_samples; ++sample) {
+    
+    for (const auto& s: corpus_sents) {
+      ctx.resize(kOrder - 1);
+      for (unsigned i = 0; i <= s.size(); ++i) {
+        WordId w = (i < s.size() ? s[i] : kEOS);
+        if (sample > 0) 
+          lm.decrement(w, ctx, eng);
+        lm.increment(w, ctx, eng);
+        ctx.push_back(w);
+      }
+    }
+
+    if (sample % 10 == 9) {
+      std::cerr << (sample + 1) << " iterations\n";
+      if (sample % 30u == 29) 
+        lm.resample_hyperparameters(eng);
+      std::cerr << " [LLH=" << lm.log_likelihood() << "]\n";
+    } else { 
+      std::cerr << '.'; 
+    }
+  }
+
+  auto tr_dur = std::chrono::steady_clock::now() - tr_start;
+  std::cerr << "Training done...time " << std::chrono::duration_cast<std::chrono::seconds>(tr_dur).count() << "s\n";  
+   
+ /*Testing */
+
+  std::string test_file = "english-wsj/english_wsj_dev.conll";
+  
+  std::vector<Words> test_sents;
+  std::vector<Words> test_tags;
+  std::vector<WxList> test_deps;
+
+  std::cerr << "Reading test corpus...\n";
+  dict.readFromConllFile(test_file, &test_sents, &test_tags, &test_deps, true);
+  std::cerr << "Corpus size: " << test_sents.size() << " sentences\n";
+
+  double llh = 0;
+  int cnt = 0;
+  
+  for (auto& s : test_sents) {
+    ctx.resize(kOrder - 1);
+    for (unsigned i = 1; i <= s.size(); ++i) {
+      WordId w = (i < s.size() ? s[i] : kEOS);
+      double lp = std::log(lm.prob(w, ctx));
+      
+      ctx.push_back(w);
+      llh -= lp;
+
+      if (i < s.size()) {
+        cnt++;
+      }
+    }
+  }
+ 
+  double cross_entropy = llh / (std::log(2) * cnt);
+  double perplexity = std::pow(2, cross_entropy);
+
+  std::cerr << "Total length: " << cnt << std::endl;
+  std::cerr << "Log prob: " << llh << std::endl;
+  std::cerr << "Cross-entropy: " << cross_entropy << std::endl;
+  std::cerr << "Perplexity: " << perplexity << std::endl;
+
+  return 0;
+}
+
---
+++ src/hpyplm/hpyplm_generate.cc
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <unordered_map>
+#include <cstdlib>
+
+#include "hpyplm.h"
+#include "corpus/corpus.h"
+#include "pyp/m.h"
+#include "pyp/random.h"
+#include "pyp/crp.h"
+#include "pyp/tied_parameter_resampler.h"
+
+#define kORDER 3  //default 4
+#define nSAMPLES 200
+
+using namespace std;
+using namespace oxlm;
+
+Dict dict;
+
+int main(int argc, char** argv) {
+  if (argc != 3) {
+    cerr << argv[0] << " <training.txt> <nsamples>\n\nEstimate a " 
+         << kORDER << "-gram HPYP LM and report perplexity\n100 is usually sufficient for <nsamples>\n";
+    return 1;
+  }
+  MT19937 eng;
+  string train_file = argv[1];
+  int samples = atoi(argv[2]);
+  
+  vector<vector<WordId> > corpuse;
+  set<WordId> vocabe, tv;
+  const WordId kSOS = dict.Convert("<s>");
+  const WordId kEOS = dict.Convert("</s>");
+  cerr << "Reading corpus...\n";
+  ReadFromFile(train_file, &dict, &corpuse, &vocabe);
+  cerr << "E-corpus size: " << corpuse.size() << " sentences\t (" << vocabe.size() << " word types)\n";
+  
+  PYPLM<kORDER> lm(vocabe.size(), 1, 1, 1, 1);
+  vector<WordId> ctx(kORDER - 1, kSOS);
+  for (int sample=0; sample < samples; ++sample) {
+    for (const auto& s : corpuse) {
+      ctx.resize(kORDER - 1);
+      for (unsigned i = 0; i <= s.size(); ++i) {
+        WordId w = (i < s.size() ? s[i] : kEOS);
+        if (sample > 0) lm.decrement(w, ctx, eng);
+        lm.increment(w, ctx, eng);
+        ctx.push_back(w);
+      }
+    }
+    if (sample % 10 == 9) {
+      cerr << " [LLH=" << lm.log_likelihood() << "]" << endl;
+      if (sample % 30u == 29) lm.resample_hyperparameters(eng);
+    } else { cerr << '.' << flush; }
+  }
+//  lm.print(cerr);
+  double llh = 0;
+  unsigned cnt = 0;
+  
+  vector<vector<WordId> > particles;
+  vector<int> length_dist;
+
+  //generate sentences
+  for (int i = 0; i < nSAMPLES; ++i) {
+    ctx.resize(kORDER - 1);
+    WordId w = kSOS;
+    
+    while (w != kEOS) {    
+      //cout << "context: ";
+      //for (auto w: ctx)
+      //  cout << w << " ";
+      
+      w = lm.generate(ctx, vocabe.size(), 0, eng); 
+      //cout << "word: " << dict.Convert(w) << endl;
+      ctx.push_back(w);
+    
+      double lp = log(lm.prob(w, ctx)) / log(2);
+      llh -= lp;
+      cnt++;
+    } 
+
+    //print the sentence
+    cout << (ctx.size() - 2) << " ";
+    for (auto w: ctx)
+        cout << dict.Convert(w) << " ";
+    cout << endl;
+    particles.push_back(ctx);
+    length_dist.push_back(ctx.size()-2);
+  } 
+
+  sort(length_dist.begin(), length_dist.end());
+  for (auto l: length_dist)
+    cout << l << " ";
+  cout << endl;
+
+  cerr << "  Log_10 prob: " << (-llh * log(2) / log(10)) << endl;
+  cerr << "        Count: " << cnt << endl;
+  cerr << "Cross-entropy: " << (llh / cnt) << endl;
+  cerr << "   Perplexity: " << pow(2, llh / cnt) << endl;
+  return 0;
+}
+
